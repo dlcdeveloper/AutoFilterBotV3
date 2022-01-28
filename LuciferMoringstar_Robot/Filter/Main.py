108a89ab5fd74ee3b86b6260cff275a7d2fefd5f
Livery Bee <94587553+LiveryBee@users.noreply.github.com>
2022-01-28 18:47:16 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -285,7 +285,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('MESSAGE OUR ADMIN', url='https://t.me/ATOM_CINEMAS_ADMIN_BOT'
+                    InlineKeyboardButton('JOIN OUR GROUP, url=f'{TUTORIAL}')
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
