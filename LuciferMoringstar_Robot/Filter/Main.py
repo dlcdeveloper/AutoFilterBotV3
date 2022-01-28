96c52f9da8b9820142f1deffc8cc7c5d61ff984d
Livery Bee <94587553+LiveryBee@users.noreply.github.com>
2022-01-28 18:53:23 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -285,7 +285,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('JOIN OUR GROUP, url=f'{TUTORIAL}')
+                    InlineKeyboardButton('JOIN OUR GROUP', url=f'{TUTORIAL}')
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
